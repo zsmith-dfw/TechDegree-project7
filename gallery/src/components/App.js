4bd00acfe1e1eb79f51b543f991387f86ef6f046
Zach Smith <rysskismith@gmail.com>
2020-08-25 21:31:23 -0500

    links were working now broken

--- gallery/src/components/App.js
+++ gallery/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import Nav from "./Nav";
-import Search from "./Search";
+// import Search from "./Search";
 import PhotoContainer from "./PhotoContainer";
 import apiKey from "./config";
 import axios from "axios";
@@ -10,26 +10,58 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      photos: [{ planes: [], trains: [], automobiles: [] }],
+      planes: [],
+    };
+    this.state = {
+      trains: [],
+    };
+    this.state = {
+      automobiles: [],
     };
   }
 
-
   componentDidMount() {
-    this.searchPhotos((this.query = "planes"));
-    this.searchPhotos((this.query = "trains"));
-    this.searchPhotos((this.query = "automobiles"));
+    this.searchPlanes();
+    this.searchTrains();
+    this.searchAutomobiles();
   }
 
-  searchPhotos = (query) => {
+  searchPlanes = (query = "planes") => {
     axios
       .get(
         `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
       )
       .then((response) => {
         this.setState({
-          // i think somewhere in this method is where we push the data back up to state
-          photos: response.data.photos.photo,
+          planes: response.data.photos.photo,
+        });
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
+  };
+  searchTrains = (query = "trains") => {
+    axios
+      .get(
+        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
+      )
+      .then((response) => {
+        this.setState({
+          trains: response.data.photos.photo,
+        });
+      })
+      .catch((error) => {
+        console.log("Error fetching and parsing data", error);
+      });
+  };
+  searchAutomobiles = (query = "automobiles") => {
+    axios
+      .get(
+        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
+      )
+      .then((response) => {
+        this.setState({
+          automobiles: response.data.photos.photo,
         });
       })
       .catch((error) => {
@@ -38,30 +70,25 @@ export default class App extends Component {
   };
 
   render() {
-    console.log(this.state.photos);
     return (
       <BrowserRouter>
         <div className="container">
-          <Search search={this.searchPhotos} />
-
+          <Nav />
           <Switch>
-            <Nav />
             <Route exact path="/" render={() => <Redirect to="/planes" />} />
             <Route
               path="/planes"
-              render={() => <PhotoContainer data={this.photos.planes} />}
+              render={() => <PhotoContainer data={this.state.planes} />}
             />
             <Route
               path="/trains"
-              render={() => <PhotoContainer data={this.photos.trains} />}
+              render={() => <PhotoContainer data={this.state.trains} />}
             />
             <Route
               path="/automobiles"
-              render={() => <PhotoContainer data={this.photos.automobiles} />}
+              render={() => <PhotoContainer data={this.state.automobiles} />}
             />
           </Switch>
-
-          <PhotoContainer data={this.state.photos} />
         </div>
       </BrowserRouter>
     );
--- gallery/src/index.js
+++ gallery/src/index.js
@@ -9,5 +9,4 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
+)
